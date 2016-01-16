cb9e122353400638bf69111ec33ca219fcdd768f
Maksim Klimov <cleemmi@gmail.com>
2016-01-15 22:44:48 -0800

    Fix import paths in CppModelManager

--- src/dubproject.cpp
+++ src/dubproject.cpp
@@ -257,25 +257,14 @@ void DubProject::appendIncludePaths(const ConfigurationInfo& info)
     CppTools::CppModelManager *modelmanager =
             CppTools::CppModelManager::instance();
     if (modelmanager) {
-        CppTools::ProjectInfo pinfo = modelmanager->projectInfo(this);
-#if QTCREATOR_MINOR_VERSION < 4
-        pinfo.clearProjectParts();
-#else
-#endif
+        CppTools::ProjectInfo pinfo = CppTools::ProjectInfo(this);
+        CppTools::ProjectPartBuilder pbuilder(pinfo);
 
-        CppTools::ProjectPart::Ptr part(new CppTools::ProjectPart);
-        part->project = this;
-        part->displayName = displayName();
+        pbuilder.setDisplayName(displayName());
+        pbuilder.setProjectFile(projectFilePath().toString());
+        auto includePaths = info.importPaths();
+        pbuilder.setIncludePaths(includePaths << projectDirectory().toString());
 
-        // This explicitly adds -I. to the include paths
-        part->projectFile = projectFilePath().toString();
-        part->headerPaths.push_back(CppTools::ProjectPart::HeaderPath(projectDirectory().toString(),
-                                                                      CppTools::ProjectPart::HeaderPath::IncludePath));
-        foreach (const QString &imp, info.importPaths()) {
-            part->headerPaths.push_back(CppTools::ProjectPart::HeaderPath(imp, CppTools::ProjectPart::HeaderPath::IncludePath));
-        }
-
-        pinfo.appendProjectPart(part);
         pinfo.finish();
         modelmanager->updateProjectInfo(pinfo);
     }

d81b16277f33053e8ed0d43608667613de2573c9
Maksim Klimov <cleemmi@gmail.com>
2016-02-06 00:07:21 -0800

    Fix building

--- build/install.sh
+++ build/install.sh
@@ -3,12 +3,12 @@
 if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
   brew install qt5
   brew link --force qt5
+
+  cd qtc_build/3.6.0/ && ln -s ./ bin
 else
   sudo apt-get install qt55-meta-full
   sudo apt-get install -qq gcc-4.8 g++-4.8 
   sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-4.8 90 
 
   . /opt/qt55/bin/qt55-env.sh
-
-  cd qtc_build/3.6.0/ && ln -s ./ bin
 fi
